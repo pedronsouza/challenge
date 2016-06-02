5a084d6602dd3d674121c05154f7a486fd05039b
Pedro Souza <pedronicholas.souza@gmail.com>
2016-06-02 04:22:28 -0300

    create progress indicator fragment

---
+++ mobile/DesafioBankFacil/app/src/main/java/br/pedronsouza/desafio/bankfacil/ui/fragments/ProgressIndicator.java
@@ -0,0 +1,11 @@
+package br.pedronsouza.desafio.bankfacil.ui.fragments;
+
+import android.support.v4.app.Fragment;
+
+import org.androidannotations.annotations.EFragment;
+
+import br.pedronsouza.desafio.bankfacil.R;
+
+@EFragment(R.layout.progress_indicator)
+public class ProgressIndicator extends Fragment {
+}
